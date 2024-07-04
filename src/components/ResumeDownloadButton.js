8a1422b81bc55c225ce5d6f7d5ebcf41bb549be5
kajal1verma <kajal5219v@gmail.com>
2024-07-04 08:56:59 +0530

    update resume file

--- src/components/ResumeDownloadButton.js
+++ src/components/ResumeDownloadButton.js
@@ -4,7 +4,7 @@ import '../css/style.css'
 const ResumeDownloadButton = () => {
     const handleDownload = () => {
         // Replace 'your_resume_path.pdf' with the actual path to your resume file
-        const resumeUrl = 'resume/kajal.pdf';
+        const resumeUrl = 'resume/kajalverma.resume.pdf';
         const anchor = document.createElement('a');
         anchor.href = resumeUrl;
         anchor.download = 'kajal_resume.pdf';
